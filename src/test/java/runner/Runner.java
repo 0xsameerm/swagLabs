0d7c09b6c2795bfc2fa31bdbabd4033cff80fb14
Mohamed M <mm6289350@gmail.com>
2025-09-28 12:44:26 +0530

    Move feature files to resources folder for running jenkins job

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,14 +1,13 @@
 package runner;
 
 
-import io.cucumber.core.backend.StepDefinition;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/features",
+        features = "src/test/resources/features",
         glue = {"stepDefinitions","hooks"},
         plugin ={"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         monochrome = true
